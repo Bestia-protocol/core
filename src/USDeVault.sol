a79d5a853cbce1a1f6112ab6b70984a61fe0bdf6
OxMarco <>
2024-04-09 23:52:15 +0200

    fix usdb backing

--- src/USDeVault.sol
+++ src/USDeVault.sol
@@ -1,68 +1,81 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
 
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IUSDe} from "./interfaces/IUSDe.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IsUSDe} from "./interfaces/IsUSDe.sol";
 import {IRouter} from "./interfaces/IRouter.sol";
 
 contract USDeVault {
-    using SafeERC20 for IUSDe;
+    using SafeERC20 for IsUSDe;
+    using SafeERC20 for IERC20;
 
-    IUSDe public immutable usde;
-    IRouter public immutable router;
+    IsUSDe public immutable susde;
+    IERC20 public immutable usde;
     address public immutable usdb;
-    address public immutable susdb; //< this may be upgradable
-    mapping(address => uint256) public stakes;
+    address public immutable susdb;
+    IRouter public immutable router;
+
+    uint256 public usdbSupply;
 
     event Stake(address indexed user, uint256 amount);
+    event StakeNative(address indexed user, uint256 amount);
     event Unstake(address indexed user, uint256 amount);
     event Rebalanced();
 
     error RestrictedToRouter();
     error InsufficientAmount();
 
-    constructor(address _router, address _usde, address _usdb, address _susdb) {
-        assert(_usde != address(0));
+    constructor(address _router, address _susde, address _usdb, address _susdb) {
         assert(_router != address(0));
-        usde = IUSDe(_usde);
+        assert(_susde != address(0));
+        assert(_usdb != address(0));
+        assert(_susdb != address(0));
         router = IRouter(_router);
+        susde = IsUSDe(_susde);
+        usde = IERC20(susde.asset());
         usdb = _usdb;
         susdb = _susdb;
     }
 
     receive() external payable {}
 
+    // sUSDe
     function stake(uint256 amount) external {
+        susde.safeTransferFrom(msg.sender, address(this), amount);
+
+        uint256 amountToMint = susde.convertToAssets(amount);
+        bytes memory data = abi.encodeWithSignature("mint(address,uint256)", msg.sender, amountToMint);
+        router.call(usdb, data);
+        usdbSupply += amountToMint;
+
+        emit Stake(msg.sender, amountToMint);
+    }
+
+    function stakeNative(uint56 amount) external {
         usde.safeTransferFrom(msg.sender, address(this), amount);
-        stakes[msg.sender] += amount;
 
         bytes memory data = abi.encodeWithSignature("mint(address,uint256)", msg.sender, amount);
         router.call(usdb, data);
+        usdbSupply += amount;
 
-        emit Stake(msg.sender, amount);
+        emit StakeNative(msg.sender, amount);
     }
 
-    // can only unstake by burning usdb on Sei
     function unstake(address to, uint256 amount) external {
         if (msg.sender != address(router)) revert RestrictedToRouter();
 
-        if (amount > stakes[to]) revert InsufficientAmount();
-        stakes[to] -= amount;
-        usde.safeTransfer(to, amount);
+        uint256 toRedeem = susde.convertToShares(amount);
+        susde.safeTransfer(to, toRedeem);
+        usdbSupply -= amount;
 
-        emit Unstake(msg.sender, amount);
+        emit Unstake(msg.sender, toRedeem);
     }
 
     function harvest() external returns (uint256) {
-        uint256 balanceBefore = usde.balanceOf(address(this));
-        usde.unstake(msg.sender);
-        uint256 balanceAfter = usde.balanceOf(address(this));
-        assert(balanceAfter > balanceBefore); // avoid rebalancing if no gains matured
-        uint256 toMint = balanceAfter - balanceBefore;
-        usde.deposit(usde.balanceOf(address(this)), address(this));
-        // send cross-chain call to mint usdb tokenx
+        // TBD
+        uint256 toMint;
 
-        // mint to the sUSBb staking vault the excess (s)USDe collected
+        // send cross-chain call to mint usdb tokenx
         bytes memory data = abi.encodeWithSignature("mint(address,uint256)", susdb, toMint);
         router.call(usdb, data);
 
---
+++ src/interfaces/IsUSDe.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.19;
+
+import {IERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+interface IsUSDe is IERC4626 {
+    function unstake(address receiver) external;
+}
