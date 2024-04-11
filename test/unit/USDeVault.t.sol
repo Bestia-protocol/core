96d90b05f828f9e5fddba05bf268c1b727b11656
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-04-11 13:43:16 +0100

    testBurnUSDbAndGetUSDe works

--- test/unit/USDeVault.t.sol
+++ test/unit/USDeVault.t.sol
@@ -32,7 +32,55 @@ contract USDeVaultTest is TestSetup {
         assertEq(userUSDbBalance, userAssets);
     }
 
+    function testUSDBMintingWithInterest() external {
+        uint256 amount = 1e18;
+        usde.mint(user, amount);
+
+        // user deposts USDe to sUSDe vault
+        vm.startPrank(user);
+        usde.approve(address(susde), amount);
+        susde.deposit(amount, user);
+        vm.stopPrank();
+
+        // more USDe is minted to sUSDe to simulate interest earned
+        usde.mint(address(susde), amount);
+
+        uint256 userShares = susde.balanceOf(user);
+        uint256 userAssets = susde.convertToAssets(userShares);
+        console2.log("user sUSDe Shares", userShares);
+        console2.log("user USDe Assets after convertToAssets()", userAssets);
+
+        vm.startPrank(user);
+        susde.approve(address(vault), amount);
+        vault.stake(susde.balanceOf(user));
+        vm.stopPrank();
+
+        uint256 userUSDbBalance = usdb.balanceOf(user);
+        console2.log("user USDb balance", userUSDbBalance);
+
+        assertEq(userUSDbBalance, userAssets);
+    }
+
     function testBurnUSDbAndGetUSDe() external {
-        // ipleemnt code here
+        uint256 amount = 1e18;
+        usde.mint(user, amount);
+
+        // user gets USDB per normal flow
+        vm.startPrank(user);
+        usde.approve(address(susde), amount);
+        susde.deposit(amount, user);
+        susde.approve(address(vault), amount);
+        vault.stake(susde.balanceOf(user));
+
+        uint256 userUSDbBalance = usdb.balanceOf(user);
+        console2.log("user USDb balance", userUSDbBalance);
+
+        // user burns USDb to get USDe
+        redeemer.burn(userUSDbBalance);
+        uint256 susdeReceivedBack = susde.balanceOf(user);
+        uint256 userUSDeBalance = susde.convertToAssets(susdeReceivedBack);
+        console2.log("user USDe balance", userUSDeBalance);
+
+        assertEq(userUSDeBalance, amount);
     }
 }
