d4ed996f3ea25d73e98dd39e0a0c9e98c1e7530a
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-04-16 15:03:34 +0100

    harvest integration test wip

--- test/integration/Harvest.t.sol
+++ test/integration/Harvest.t.sol
@@ -8,40 +8,37 @@ contract HarvestTest is TestSetup {
     // tests a user deposits USDe to sUSDe vault and the stakes USDB
     // then add yield on sUSDe and harvest to USDB
 
-    function testStakeAndHarvest() public {
+    function testStakeUsdbAndHarvest() public {
         uint256 amount = 1e18;
         uint256 yield = 1e16; // 1% yield
         usde.mint(user, amount);
 
-        console2.log(
-            "starting usdb balance of susdb",
-            usdb.balanceOf(address(susdb))
-        );
+        vm.startPrank(user);
 
         // user deposits USDe to sUSDe vault and stakes to get USDB
-        vm.startPrank(user);
         susde.deposit(amount, user);
         vault.stake(susde.balanceOf(user));
 
-        usdb.approve(address(susdb), usdb.balanceOf(user));
+        // user deposits received USDB to sUSDb
+        uint256 userStartingBalance = usdb.balanceOf(user);
+        usdb.approve(address(susdb), userStartingBalance);
         susdb.deposit(usdb.balanceOf(user), user);
-        console2.log(
-            "usdb balance of susdb after user deposit",
-            usdb.balanceOf(address(susdb))
-        );
-        console2.log("susdb balance of user", susdb.balanceOf(user));
 
         vm.stopPrank();
 
-        // add yield to sUSDe
+        // add yield to sUSDe harvest yield to USDB
         usde.mint(address(susde), yield);
-
-        // harvest yield to USDB
         vault.harvest();
 
-        console2.log(
-            "usdb balance of susdb after harvest",
-            usdb.balanceOf(address(susdb))
-        );
+        // user available usdb balance should be increased by the yield
+        uint256 userEndingBalance = usdb.balanceOf(address(susdb));
+
+        console2.log("userStartingBalance", userStartingBalance);
+        console2.log("userEndingBalance", userEndingBalance);
+
+        uint256 interestEarned = userEndingBalance - userStartingBalance;
+
+        // assert the user has received the correct usdb yield after rounding
+        assertEq(interestEarned + 1, yield);
     }
 }
