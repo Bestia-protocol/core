22fc8cc07b23018dc7e256187f40593943575742
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-04-16 15:51:55 +0100

    user flow from start to finish working, with small rouding

--- test/integration/Harvest.t.sol
+++ test/integration/Harvest.t.sol
@@ -5,8 +5,9 @@ import {TestSetup} from "../TestSetup.t.sol";
 import {console2} from "forge-std/Test.sol";
 
 contract HarvestTest is TestSetup {
-    // tests a user deposits USDe to sUSDe vault and the stakes USDB
-    // then add yield on sUSDe and harvest to USDB
+    // 1. a user deposits USDe to sUSDe vault and the stakes USDB
+    // 2. then add yield on sUSDe and harvest to USDB
+    // 3. the user unstakes and withdraws to USDe
 
     function testStakeUsdbAndHarvest() public {
         uint256 amount = 1e18;
@@ -20,8 +21,8 @@ contract HarvestTest is TestSetup {
         vault.stake(susde.balanceOf(user));
 
         // user deposits received USDB to sUSDb
-        uint256 userStartingBalance = usdb.balanceOf(user);
-        usdb.approve(address(susdb), userStartingBalance);
+        uint256 userUSDbStartingBalance = usdb.balanceOf(user);
+        usdb.approve(address(susdb), userUSDbStartingBalance);
         susdb.deposit(usdb.balanceOf(user), user);
 
         vm.stopPrank();
@@ -33,12 +34,34 @@ contract HarvestTest is TestSetup {
         // user available usdb balance should be increased by the yield
         uint256 userEndingBalance = usdb.balanceOf(address(susdb));
 
-        console2.log("userStartingBalance", userStartingBalance);
+        console2.log("userStartingBalance", userUSDbStartingBalance);
         console2.log("userEndingBalance", userEndingBalance);
 
-        uint256 interestEarned = userEndingBalance - userStartingBalance;
-
         // assert the user has received the correct usdb yield after rounding
+        uint256 interestEarned = userEndingBalance - userUSDbStartingBalance;
         assertEq(interestEarned + 1, yield);
+
+        vm.startPrank(user);
+
+        // user unstakes and withdraws to USDe
+        susdb.redeem(susdb.balanceOf(user), user, user);
+        uint256 usdbBalance = usdb.balanceOf(user);
+        console2.log("usdb.balanceOf(user) after redeem", usdbBalance);
+        
+        // burn the USDB to receive sUSDe
+        redeemer.burn(usdbBalance);
+        uint256 susdeBalance = susde.balanceOf(user);
+        console2.log("susde.balanceOf(user) after burning USDb", susdeBalance);
+
+        // redeem sUSDe to USDe
+        susde.redeem(susde.balanceOf(user), user, user);
+        uint256 usdeBalance = usde.balanceOf(user);
+        console2.log("usde.balanceOf(user) after redeem", usdeBalance);
+
+        vm.stopPrank();
+
+        // assert the user has received the correct usde balance after rounding
+        uint256 tolerance = 3;
+        assertApproxEqAbs(usdeBalance, amount + yield, tolerance);
     }
 }
