cf385e0ebb48df564cab4083cff0c3fbe3f537f2
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-04-18 12:57:08 +0100

    removed redundant IERC20 interface from StakedUSDb, added test that token is not transferrable

--- test/unit/StakedUSDb.t.sol
+++ test/unit/StakedUSDb.t.sol
@@ -25,4 +25,27 @@ contract StakedUSDbTest is TestSetup {
 
         assertEq(userShares, userBalance);
     }
+
+    // test nonTransferable
+    function testNonTransferable() external {
+        uint256 amount = 1e18;
+
+        // router mints usdb to user
+        vm.startPrank(address(router));
+        usdb.mint(user, amount);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+
+        usdb.approve(address(susdb), amount);
+        susdb.deposit(usdb.balanceOf(user), user);
+
+        susdb.approve(address(user2), amount);
+
+        // user2 tries to transfer sUSDb from user
+        vm.expectRevert();
+        susdb.transfer(user2, amount);
+
+        vm.stopPrank();
+    }
 }
