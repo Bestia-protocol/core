5dc16fe160b7df6b6364330880b43bf37cd7b76a
OxMarco <>
2024-04-27 10:45:56 +0200

    remove failing file

--- test/mock/mockUSDe.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.19;
-
-import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
-
-contract MockUSDe is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("USDe", "USDe") {}
-}
